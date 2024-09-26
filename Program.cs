83c87c56e3c4218e01090b7aa4fe9c2061597cbc
Daniel <donybof1@gmail.com>
2024-09-26 13:05:02 +0300

    Finished Mission

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+class Program
+{
+    public static void Main()
+    {
+        Queue queue = new();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(1);
+    }
+
+    public static Queue ReversedQueue(Queue Q1)
+    {
+        Queue Reversed = new();
+        Stack temp = new();
+        while (Q1.Count > 0)
+        {
+            temp.Push(Q1.Dequeue());
+        }
+        while (temp.Count > 0)
+        {
+            Reversed.Enqueue(temp.Pop());
+        }
+        return Reversed;
+    }
+
+    public static bool IsPalindrome(Queue Q1)
+    {
+        Queue Reversed = ReversedQueue(Q1);
+        while (Q1.Count > 0)
+        {
+            if (Q1.Dequeue() != Reversed.Dequeue()) { return false; }
+        }
+        return true;
+    }
+}
